a948201fb02b9cfa835a5537a108b156aaacd246
Nweke Chidi <nwekechidigodwin460@gmail.com>
2022-06-27 21:48:00 +0100

    Tetsform3
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
   $visitor_email = $_POST['email'];
 
 
-  $email_from = 'nwekechidigodwin460@gmail.com';
+  $email_from = 'hello@uncloudy.co';
   $email_subject = "UNCLOUDY NOTIFICATION";
   $email_body = "You have received a waitlist vsitor email:\n".
                           "Here is the email:\n $visitor_email".
@@ -11,27 +11,14 @@
     
     $to = "$visitor_email";
 
-    $headers = "From: $email_from \r\n";
+    $headers = "From: $email_from";
     
-    $headers .= "Reply-To: $visitor_email \r\n";
-    if($email!=NULL){
+    // $headers .= "Reply-To: $visitor_email \r\n";
+
+    if($visitor_email!=NULL){
         mail($to,$email_subject,$email_body,$headers);
     }
     //redirect
     header("Location:index.html");
-    
-    
-
-// if($_POST["message"]) {
-
-
-// mail("nwekechidigodwin460@gmail.com", "UNCLOUDY NOTIFICATION",
-
-
-// $_POST["You have successfully been added to unclougy waitlist"]. "From: chidin@cedarview.ng");
-
-
-// }
-
 
 ?>
